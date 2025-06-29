08d9382819de5a23290d436a2ed28abf8a5dfd9f
3788-lacarrillo6 <118786400+3788-lacarrillo6@users.noreply.github.com>
2025-06-29 12:38:13 -0500

    simuladorFor

--- Agente/client/src/components/SimulacionForm.tsx
+++ Agente/client/src/components/SimulacionForm.tsx
@@ -14,6 +14,7 @@ const SimulacionForm: React.FC<{ onCreated: () => void; onCancel: () => void }> 
     descripcion: "",
     objetivo: "",
     escenarios: [],
+    usuario_id: ""
   });
 
   const handleAddPaso = () => {
